3879fb66944c949516ade1968cfc7c1d96c6665b
Kim Arum <83346234+kimrumm@users.noreply.github.com>
2022-10-05 22:24:06 +0900

    [unrated] Title: 배열의 평균값, Time: 0.04 ms, Memory: 71.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double solution(int[] numbers) {
+        
+        double sum = 0.0;
+        
+        for(double num : numbers){
+            sum += num;
+        }
+        
+        double answer = sum / numbers.length;
+        
+        return answer;
+    }
+}
