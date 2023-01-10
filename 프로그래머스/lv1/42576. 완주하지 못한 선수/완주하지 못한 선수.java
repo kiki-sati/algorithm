b97eb5ee87e287c7c21a3980d5c34d328ed9e4bc
Kim Arum (sati) <83346234+kimrumm@users.noreply.github.com>
2023-01-10 23:57:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.58 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,24 +4,21 @@ import java.util.Map;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-
         HashMap<String, Integer> map = new HashMap<>();
+
         for (String player : participant) {
             map.put(player, map.getOrDefault(player, 0) + 1);
         }
 
         for (String player : completion) {
-            map.put(player, map.get(player) - 1);
-
-            for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                if (entry.getValue() != 0) {
-                    answer = entry.getKey();
-                    break;
-                }
+            int count = map.get(player);
+            if (count > 1) {
+                map.put(player, count - 1);
+            } else {
+                map.remove(player);
             }
         }
 
-        return answer;
+        return map.keySet().iterator().next();
     }
 }
